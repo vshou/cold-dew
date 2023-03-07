812f35357093c1795060b02d8915fd1df0380020
withv <vshowc@outlook.com>
2023-03-07 19:01:55 +0800

    音乐播放组件设计

---
+++ src/components/MusicComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="common_div" title="播放音乐">
+    <video loop
+           ref="music_video"
+           :class="{'bg_dy': is_play}"
+           :style="{ 'background-image': 'url(' + music_bg +')' }"
+           :src="music_url"
+           @click="operationFun">
+    </video>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { HTTP_IMG_ADDR, HTTP_MUSIC_ADDR } from "../constants/constant";
+
+
+  const music_video = ref()
+  onMounted(() => {
+    music_video.value.focus()
+  })
+  const music_url = ref<string>(HTTP_MUSIC_ADDR + 'bgm.ogg')
+  const music_bg = ref<string>(HTTP_IMG_ADDR + "music_bg.png")
+  const is_play = ref<boolean>(false)
+  const operationFun = () => {
+    if (is_play.value) {
+      is_play.value = false
+      music_video.value.pause()
+      return
+    }
+    is_play.value = true
+    music_video.value.play()
+  }
+</script>
+
+<style lang="less" scoped>
+  .common_div {
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    border-radius: 50%;
+    overflow: hidden;
+    video {
+      background-size: cover;
+    }
+  }
+  .bg_dy {
+    animation: bg_rotate 3s linear 0s infinite;
+  }
+  @keyframes bg_rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
