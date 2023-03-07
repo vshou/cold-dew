428b09e4a907d684d2ac45463c2a2e8ad87b70d4
withv <vshowc@outlook.com>
2023-03-07 19:07:14 +0800

    vue 全局截流设计

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App);
+
+
+app.directive('throttle', (el, binding) => {
+    el.addEventListener('click', () => {
+        el.style.pointerEvents = 'none'
+        if (!el.disabled) {
+            setTimeout(() => {
+                el.style.pointerEvents = 'auto'
+            }, binding.value || 1000)
+        }
+    })
+})
+
+app.use(router).use(store).mount('#app')
