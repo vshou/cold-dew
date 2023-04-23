644c5cc2072bde86449c2bc606f37f4b857836f2
vshowc <vshowc@outlook.com>
2023-04-23 15:16:02 +0800

    更新

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -2,8 +2,7 @@
   <div class="common_div fixed_currency" :style="{ 'z-index': sign ? 999 : -999 }">
     <div :class="{ 'shade_div': true, 'fixed_currency': true, 'show_currency': load }">
       <div class="load_img_div center_currency">
-<!--        <img :class="{ 'load_img': true, 'load_img_rotate': sign }" src="../assets/image/loading.png"/>-->
-        <div class="loader"></div>
+        <img :class="{ 'load_img': true, 'load_img_rotate': sign }" src="../assets/image/loading.png" alt="加载..."/>
       </div>
     </div>
   </div>
@@ -33,7 +32,7 @@ import { ref, watch } from 'vue'
 
 <style lang="less" scoped>
   .shade_div {
-    background: #000000;
+    background: rgba(0, 0, 0, .6);
     opacity: 0;
     transition: .4s;
     .load_img_div {
@@ -41,36 +40,6 @@ import { ref, watch } from 'vue'
       .load_img {
         width: 55px;
         height: 55px;
-      }
-      .loader {
-        width: 150px;
-        height: 150px;
-        border-radius: 50%;
-        background: linear-gradient(45deg, transparent, transparent 40%, #e5f403);
-        animation: rotate_action 2s linear 0s infinite;
-      }
-      .loader::before {
-        content: "";
-        position: absolute;
-        top: 6px;
-        left: 6px;
-        right: 6px;
-        bottom: 6px;
-        background: #000000;
-        z-index: 3;
-        border-radius: 50%;
-      }
-      .loader::after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: linear-gradient(45deg, transparent, transparent 40%, #e5f403);
-        border-radius: 50%;
-        z-index: 2;
-        filter: blur(20px);
       }
     }
   }
